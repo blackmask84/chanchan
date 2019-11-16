2ea95d617c985a67e5b9506ce38412859a6281e0
Truly <Nguyen24384@gmail.com>
2019-11-16 14:22:59 +0700

    submit

--- app.js
+++ app.js
@@ -47,6 +47,9 @@ app.get('/timhieuchuongtrinh', function (req, res) {
 app.get('/chancode', function (req, res) {
     res.sendFile(__dirname + '/public/10_programcreat.html');
 });
+app.get('/moiquanhetuyetvoi', function (req, res) {
+    res.sendFile(__dirname + '/public/moiquanhetuyetvoi.html');
+});
 
 // get form
 
