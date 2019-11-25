34424e797922a0e33d51213e7beaee64e67646fb
Truly <Nguyen24384@gmail.com>
2019-11-25 11:17:07 +0700

    submit

--- public/form/js/form1/apps/form1.js
+++ public/form/js/form1/apps/form1.js
@@ -31,10 +31,10 @@ $(document).ready(function() {
         // Open Info popup
         $(formId + '.js_info_open').on('click', function() {
             if ($(this).hasClass('btn_register_more')) {
-                $(formId + '#choose_type').text('can tu van');
+                $(formId + '#choose_type').text('nam');
             }
             if ($(this).hasClass('btn_register_now')) {
-                $(formId + '#choose_type').text('dang ky ngay');
+                $(formId + '#choose_type').text('nu');
             }
             $(formId).find('.active').removeClass('active');
             $(formId + '.popup.popup_info').addClass('active');
