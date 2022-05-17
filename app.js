54c6e497c51b5b1cade77ccfee687e6a64387973
Truly <Nguyen24384@gmail.com>
2022-05-17 22:43:34 +0700

    up

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 app.get('/trialm%', function (req, res) {
-    res.sendFile(__dirname + '/public/tangtoc.html');
+    res.sendFile(__dirname + '/public/dangky.html');
 });
 app.get('/trial%', function (req, res) {
-    res.sendFile(__dirname + '/public/tangtoc.html');
+    res.sendFile(__dirname + '/public/dangky.html');
 });
 app.get('/openletter%', function (req, res) {
-    res.sendFile(__dirname + '/public/tangtoc.html');
+    res.sendFile(__dirname + '/public/dangky.html');
 });
 app.get('/matchguide%', function (req, res) {
     res.sendFile(__dirname + '/public/match_guide.html');
@@ -36,50 +36,34 @@ app.get('/newsignpolicy%', function (req, res) {
     res.sendFile(__dirname + '/public/newsign_policy.html');
 });
 app.get('/paidset%', function (req, res) {
-    res.sendFile(__dirname + '/public/paidset.html');
+    res.sendFile(__dirname + '/public/dangky.html');
 });
 app.get('/paidgui%', function (req, res) {
     res.sendFile(__dirname + '/public/paidgui.html');
 });
 app.get('/tangtoccamp', function (req, res) {
-    res.sendFile(__dirname + '/public/tangtoc.html');
+    res.sendFile(__dirname + '/public/dangky.html');
 });
 app.get('/tangtoc%', function (req, res) {
-    res.sendFile(__dirname + '/public/tangtoc.html');
+    res.sendFile(__dirname + '/public/dangky.html');
 });
 app.get('/tangtoc', function (req, res) {
-    res.sendFile(__dirname + '/public/tangtoc.html');
+    res.sendFile(__dirname + '/public/dangky.html');
 });
 app.get('/tangtocdk', function (req, res) {
-    res.sendFile(__dirname + '/public/tangtocdk.html');
+    res.sendFile(__dirname + '/public/dangky.html');
 });
 app.get('/hengiaumat', function (req, res) {
-    res.sendFile(__dirname + '/public/tangtoc.html');
+    res.sendFile(__dirname + '/public/hengiaumat.html');
 });
 app.get('/hengiaumat%', function (req, res) {
-    res.sendFile(__dirname + '/public/tangtoc.html');
+    res.sendFile(__dirname + '/public/hengiaumat.html');
 });
-
-// get form
-app.get('/believe', function (req, res) {
-    res.sendFile(__dirname + `/public/form/${req.originalUrl}.html`);
+app.get('/dangky', function (req, res) {
+    res.sendFile(__dirname + '/public/dangky.html');
 });
-
-// load facebook
-app.get('/facebook1', function (req, res) {
-    res.sendFile(__dirname + `/public/form/${req.originalUrl}.html`);
-});
-
-// handle register form
-app.post('/register', function (req, res) {
-    const data = req.body;
-    axios.post('http://localhost:1337/registers', data)
-    .then(function (data) {
-        res.status(200).send('Register success');
-    }).catch(function (err) {
-        console.log(err);
-        res.status(400).send('Register failed')
-    })
+app.get('/dangky%', function (req, res) {
+    res.sendFile(__dirname + '/public/dangky.html');
 });
 
 var server = app.listen(8888, function(){
